e7d2d52c30dcf315983030aec576b8e5ba6ac49d
Claudi0-V <claudiov.dejesus@protonmail.com>
2022-09-22 20:26:16 -0300

    adds user input to app

--- ex01/src/main.rs
+++ ex01/src/main.rs
@@ -1,8 +1,25 @@
+use std::io;
+
 fn main() {
-    print_number(32);
-    print_number(74);
+    let input = get_input();
+    let input = input.trim();
+
+    match input.parse::<i32>() {
+        Ok(val) => print_number(val),
+        Err(_) => println!("{input} is not a valid number, try again."),
+    }
 }
 
-fn print_number(num: u32) {
+fn get_input() -> String {
+    let mut input = String::new();
+
+    println!("Type a number:");
+    io::stdin()
+        .read_line(&mut input)
+        .expect("something went wrong...");
+    input
+}
+
+fn print_number(num: i32) {
     println!("\nthe number entered was {num}");
 }
